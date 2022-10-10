51c22ff4cf6b6e37a279d3bd1a747e5292db7a82
alberola <93872954+alberola@users.noreply.github.com>
2022-10-10 14:07:50 +0200

    update

--- Ejercicio12/index.php
+++ Ejercicio12/index.php
@@ -4,51 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Iniciar Sesion</title>
 </head>
 <body>
-    <h1>Alejandro Alberola Login</h1>
-    <div> 
-        <?php
+    <?php
 
-            function clean($data){
-                $data = trim($data);
-                $data = htmlentities($data);
-                $data = htmlspecialchars($data);
-                $data = stripslashesip($data);
-                return $data;
-            }
+        function clean($data){
+            $data = trim($data);
+            $data = htmlentities($data);
+            $data = htmlspecialchars($data);
+            $data = stripslashesip($data);
+            return $data;
+        }
 
-            $_SESSION[$dates] = array(
-                array('admin','1234','admin'),
-                array('alejandro','1234','admin'),
-                array('ale','qwerty','usuario')
-            );
-            if (isset($_POST['user']) && isset($_POST['password']) ){        
+        $_SESSION["$dates"] = array(
+            array('admin','1234','admin'),
+            array('alejandro','1234','admin'),
+            array('ale','qwerty','usuario')
+        );
+        if (isset($_POST['user']) && isset($_POST['password']) ){        
             $user = clean($_POST['user']);
             $password = clean($_POST['password']);
             for ( $i = 0 ; i < count($dates) ; $i++){
-                if ($user = $dates[$i][0] && $password != $dates[$i][1]){
-
+                if ($user != $dates[$i][0] && $password != $dates[$i][1]){
+                    echo "Incorrect log in please register first.";
                 } else {
                     echo "";
                 }
             }
-            
-            }
+        }
 
-        ?>     
-        <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>" method="post">
-            <input type="text" name="user" placeholder="alejandro" autocomplete="off"><br><br>
-            <input type="password" placeholder="1234" name="password"><br><br>
-            <input type="submit" value="Sign in">
-            <input type="submit" value="Register">
-            <input type="reset" value="Reset">
-
-            <br><br>
-            <button><a href="http://localhost/DSW/">Back</a></button>
-        </form>
-    </div>
+    ?> 
+    <button class="back"><a href="http://localhost/DSW/">Back</a></button>
+    <form action="">
+        <h1>Alejandro Alberola Log in</h1>
+        <input type="text" placeholder="alejandro" name="user">
+        <input type="password" placeholder="1234" name="password">
+        <button>Log in</button>
+        <p class="link"><a href="register.php">Don't have an account yet?</a></p>
+    </form>
 </body>
 </html>
---
+++ Ejercicio12/register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="registerStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>Formulario de Registro</title>
+</head>
+<body>
+    <?php
+
+    function clean($data){
+        $data = trim($data);
+        $data = htmlentities($data);
+        $data = htmlspecialchars($data);
+        $data = stripslashesip($data);
+        return $data;
+    }
+
+    $_SESSION["$dates"] = array(
+        array('admin','1234','admin'),
+        array('alejandro','1234','admin'),
+        array('ale','qwerty','usuario')
+    );
+    if (isset($_POST['user']) && isset($_POST['password']) ){        
+        $user = clean($_POST['user']);
+        $password = clean($_POST['password']);
+        for ( $i = 0 ; i < count($dates) ; $i++){
+            if ($user != $dates[$i][0] && $password != $dates[$i][1]){
+                echo "Incorrect log in please register first.";
+            } else {
+                echo "";
+            }
+        }
+    }
+    ?>
+
+    <form action="">
+        <h1>Register Form</h1>
+        <input type="text" placeholder="Insert your user" name="user">
+        <input type="password" placeholder="Insert your password" name="password">
+        <p class="terms">I agree with terms and conditions</p>
+        <button>Registrarse</button>
+        <p class="link"><a href="index.php">Do you already have an account?</a></p>
+    </form>
+</body>
+</html>
